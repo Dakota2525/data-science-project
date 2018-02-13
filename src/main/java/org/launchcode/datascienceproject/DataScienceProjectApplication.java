79912c4e43ef00dcf519842beee9c8e1237094fe
Dakota2525 <malloydaks@gmail.com>
2018-02-13 09:42:32 -0600

    Data Science Project

---
+++ src/main/java/org/launchcode/datascienceproject/DataScienceProjectApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.datascienceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScienceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScienceProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/org/launchcode/datascienceproject/DataScienceProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.datascienceproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataScienceProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
